6ef7d5b09a9b10f8d35700101ff2a9c3f43646db
Madison Harris <madisonharris@My-MacBook.local>
2023-12-07 20:13:11 -0700

    updated climate data with linear regresison analysis notes and understanding

--- ecology class, climate data.R
+++ ecology class, climate data.R
@@ -1,6 +1,9 @@
 ###########################
 ## ecology class, climate data
 
+## tree core data
+## 
+
 ####
 
 ## is there a significant change in mean annual temperature between 1990-2005 & 2006-2021
@@ -27,15 +30,16 @@ ggplot(ice, aes(x=temp, y=co2))+
 
 
 ####### tree core data
+getwd()
 
-tree_core = read.csv("bear_basin_snotel_tree_width.csv")
+tree_core = read.csv("homework/bear_basin_snotel_tree_width.csv")
 View(tree_core)
 names(tree_core)
 
 ## graph to compare core width and cum precip
 precip = ggplot(tree_core, aes(x=ring_width, y=cum_precip))+
   geom_point(size=2)+
-  labs(x="Core Width (mm)", y="Cumulative Precipitarion")+
+  labs(x="Core Width (mm)", y="Cumulative Precipitarion (cm)")+
   geom_smooth(method = "lm", se = FALSE)+
   theme_classic()
 precip
@@ -68,7 +72,7 @@ all_together
 ## graph to compare core width and cum precip
 precip = ggplot(tree_core, aes(y=ring_width,x=cum_precip))+
   geom_point(size=2)+
-  labs(y="Core Width (mm)", x="Cumulative Precipitarion")+
+  labs(y="Core Width (mm)", x="Cumulative Precipitarion (cm)")+
   geom_smooth(method = "lm", se = FALSE)+
   theme_classic()
 precip
@@ -76,7 +80,7 @@ precip
 ## graph to compare core width and max swe
 swe = ggplot(tree_core, aes(y=ring_width, x=max_swe))+
   geom_point(size=2)+
-  labs(y="Core Width (mm)", x="Maximum Snow Water Equivalent")+
+  labs(y="Core Width (mm)", x="Maximum SWE (cm)")+
   geom_smooth(method = "lm", se = FALSE)+
   theme_classic()
 swe
@@ -90,6 +94,35 @@ temp = ggplot(tree_core, aes(y=ring_width, x=mean_temp))+
 temp
 
 head(tree_core)
+
+
+# ggplotRegression(lm(Y value ~ X value, data = dataframe))
+ggplotRegression(lm(ring_width ~ cum_precip, data = tree_core))
+
+library(ggplot2)
+
+ggplotRegression <- function (fit) {
+  
+  require(ggplot2)
+  
+  ggplot(fit$model, aes_string(x = names(fit$model)[2], y = names(fit$model)[1])) + 
+    geom_point() +
+    stat_smooth(method = "lm", col = "red") +
+    labs(title = paste("Adj R2 = ",signif(summary(fit)$adj.r.squared, 5),
+                       "Intercept =",signif(fit$coef[[1]],5 ),
+                       " Slope =",signif(fit$coef[[2]], 5),
+                       " P =",signif(summary(fit)$coef[2,4], 5)))
+}
+
+
+
+
+
+
+
+
+
+citation(package = "ggplot2")
 
 library(cowplot)
 all_together = plot_grid(temp, swe, precip, ncol=3, nrow = 1)
@@ -112,10 +145,13 @@ model = lm(ring_width ~ mean_temp, data = tree_core)
 summary(model)
 
 ##### multiple regression analysis
-model_3 = lm(ring_width ~ mean_temp + cum_precip + snow_disap, data = tree_core)
+model_2 = lm(ring_width ~ mean_temp + cum_precip + max_swe, data = tree_core)
+summary(model_2)
 summary(model_3)
 
+plot(model_2, 5)
 
+View(tree_core)
 ## the later the snow melts, the more growth we see. Slope of 4.174e-03
 
 snow_disap_core = ggplot(tree_core, aes(x=ring_width, y = snow_disap))+
@@ -127,3 +163,4 @@ snow_disap_core
 plot(model_3)
 
 par(mfrow=c(2,2))
+plot(model_2)
